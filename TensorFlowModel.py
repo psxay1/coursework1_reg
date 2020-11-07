ca7efbed24b530d1ea58b249eca7e53f3484a2b2
Aayush Pandey <73607287+Invoker8899@users.noreply.github.com>
2020-11-07 12:46:56 +0000

    TensorFlowModel.py please check the output with modified layers and nodes also added gradient descent as optimizer not adam as suggested by dion..
    I hope this would be fine...

--- TensorFlowModel.py
+++ TensorFlowModel.py
@@ -11,25 +11,24 @@ x_test_scale = pp.x_test
 y_train_scale = pp.y_train
 y_test_scale = pp.y_test
 
-
-model = tf.keras.models.Sequential()  # using pre-defined keras sequential model
+# using pre-defined keras sequential model
+model = tf.keras.models.Sequential()
+# defining the input layer 1 of the Neural network
 model.add(Dense(11, input_dim=11, kernel_initializer='normal', activation='relu'))
-# defining the input layer with 11 input nodes for neural network
+# defining the input layer 2 of the Neural network
 model.add(Dense(45, activation='relu'))
-# defining hidden layer 1 with 2670 nodes
+# defining the input layer 3 of the Neural network
 model.add(Dense(25, activation='relu'))
+# defining the input layer 4 of the Neural network i.e. output node
 model.add(Dense(1, activation='relu'))
-# defining output node, with one output node for neural network
-model.summary()
 # gives a synopsis of our neural network model
-
-# optimization function is defined, using sgd: gradient descent optimizer with learning rate, in built optimizer used for regression
+model.summary()
+# optimization function is defined, using sgd: gradient descent optimizer with learning rate
 sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
 # cost function is defined using mean square error mse, and mean absolute error mae
-
 model.compile(loss='mse', optimizer='sgd', metrics=['mse', 'mae', 'accuracy'])
-history = model.fit(x_train_scale, y_train_scale, epochs=1000, batch_size=100, verbose=1, validation_split=0.2)
 # training the model
+history = model.fit(x_train_scale, y_train_scale, epochs=1000, batch_size=100, verbose=1, validation_split=0.2)
 predictions = model.predict(x_test_scale)
 print(history.history.keys())
 # "Loss" plot for loss vs epoch, gives us information about accuracy
