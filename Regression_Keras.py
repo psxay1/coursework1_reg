857da6a78d12a067fa710b209a2c3ad1b4d28291
Akshay <psxay1@nottingham.ac.uk>
2020-11-10 07:43:24 +0000

    Updated preprocessing and Regression_Keras for kFold cross validation. Final regression model.

--- Regression_Keras.py
+++ Regression_Keras.py
@@ -3,36 +3,50 @@ from keras.optimizers import SGD
 import preprocessing as pp
 from matplotlib.pyplot import ylabel, plot, legend, show, xlabel, title
 from tensorflow.python.keras.layers import Dense
+from sklearn.model_selection import KFold
+import  numpy as np
 # preprocessing.py is common processing file for all models in this project.. the data is split and normalized
-# normalized values are assigned to the model
-x_train_scale = pp.x_train
-x_test_scale = pp.x_test
-y_train_scale = pp.y_train
-y_test_scale = pp.y_test
-# using pre-defined keras sequential model
-model = tf.keras.models.Sequential()
-# defining the input layer 1 of the Neural network
-model.add(Dense(11, input_dim=11, kernel_initializer='normal', activation='relu'))
-# defining the input layer 2 of the Neural network
-model.add(Dense(45, activation='relu'))
-# defining the input layer 3 of the Neural network
-model.add(Dense(25, activation='relu'))
-# defining the input layer 4 of the Neural network i.e. output node
-model.add(Dense(1, activation='relu'))
-# gives a synopsis of our neural network model
-model.summary()
-# optimization function is defined, using sgd: gradient descent optimizer with learning rate
-sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
 
 tf.keras.backend.set_epsilon(1)
 
 # cost function is defined using mean square error mse, and mean absolute error mae
-model.compile(loss='mse', optimizer='sgd', metrics=['mse', 'mae', 'mean_absolute_percentage_error', 'mean_squared_logarithmic_error'])
-# training the model
-history = model.fit(x_train_scale, y_train_scale, validation_data=(x_test_scale, y_test_scale), epochs=1000,
-                    batch_size=150, verbose=1, validation_split=0.2)
-predictions = model.predict(x_test_scale)
-print(history.history.keys())
+kf = KFold(n_splits=10, shuffle=True)
+
+features = pp.kFold_features
+labels = pp.kFold_labels
+scores = []
+x_test_arr = []
+y_test_arr = []
+
+for train_index, test_index in kf.split(features):
+    x_train, x_test, y_train, y_test = features[train_index], features[test_index], labels[train_index], labels[test_index]
+
+    # using pre-defined keras sequential model
+    model = tf.keras.models.Sequential()
+    # defining the input layer 1 of the Neural network
+    model.add(Dense(11, input_dim=11, kernel_initializer='normal', activation='relu'))
+    # defining the input layer 2 of the Neural network
+    model.add(Dense(45, activation='relu'))
+    # defining the input layer 3 of the Neural network
+    model.add(Dense(25, activation='relu'))
+    # defining the input layer 4 of the Neural network i.e. output node
+    model.add(Dense(1, activation='relu'))
+    # gives a synopsis of our neural network model
+    model.summary()
+    # optimization function is defined, using sgd: gradient descent optimizer with learning rate
+    sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+
+    model.compile(loss='mse', optimizer='sgd',
+                  metrics=['mse', 'mae', 'mean_absolute_percentage_error', 'mean_squared_logarithmic_error'])
+    x_test_arr.append(x_test)
+    y_test_arr.append(y_test)
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=35,
+                              batch_size=100, verbose=0, validation_split=0.4)
+    score = model.evaluate(x_test, y_test)
+    scores.append(score)
+
+print("Lasr: ", scores[-1])
+
 # "Loss" plot for loss vs epoch, gives us information about accuracy
 plot(history.history['loss'])
 plot(history.history['val_loss'])
--- preprocessing.py
+++ preprocessing.py
@@ -1,37 +1,18 @@
 import data_loader as dl
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-data = dl.load_data('data/winequality-red.csv')  # import data file
+# data = dl.load_data('data/winequality-red.csv')  # import data file
+data = dl.load_data('data/winequality-white.csv')  # import data file
 df0 = dl.into_dataframe(data)  # change imported data into dataframe
 df0_features = dl.get_features(df0, 'quality')
 df0_labels = dl.get_labels(df0, 'quality')
 
-# diving feature data into feature_train & feature_test
-
-x = df0_features.to_numpy()
-
-y = df0_labels.to_numpy().reshape(-1, 1)
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=27)
-
-# normalizing feature_train & feature_test
-
-# fit scaler on training data
-
-norm_x = MinMaxScaler(feature_range=(0, 1)).fit(x_train)
-
-# transform training data
-
-x_train = norm_x.transform(x_train)
-
-
-# transform testing dataa
-
-x_test = norm_x.transform(x_test)
-
-norm_y = MinMaxScaler(feature_range=(0, 1)).fit(y_train)
-
-y_train = norm_y.transform(y_train)
-
-y_test = norm_y.transform(y_test)
+#-----------------------------------------------------------------------------
+# data for kFold
+df_temp = df0
+features = df0_features.to_numpy()
+kFold_features = MinMaxScaler().fit_transform(features)
+labels = df0_labels.to_numpy().reshape(-1, 1)
+kFold_labels = MinMaxScaler().fit_transform(labels)
+print("jhseojbnsdkj")
